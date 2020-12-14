233cdcf4d9631e647bcb9a812e8cf4093f70e398
Scott <53128969+Scrubaducious@users.noreply.github.com>
2020-12-14 18:10:10 -0500

    Policy-based authorization added

---
+++ BugTracker/AuthorizationRequirements/NameRequirement.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BugTracker.AuthorizationRequirements
+{
+    public class NameRequirement : IAuthorizationRequirement
+    {
+        public NameRequirement(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    public class NameRequirementHandler : AuthorizationHandler<NameRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, NameRequirement requirement)
+        {
+            var firstName = context.User.FindFirst("FirstName").Value;
+            if (firstName == requirement.Name)
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BugTracker/CustomUserClaimsPrincipalFactory.cs
@@ -0,0 +1,30 @@
+﻿using BugTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BugTracker
+{
+    public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+    {
+        public CustomUserClaimsPrincipalFactory(
+            UserManager<ApplicationUser> _userManager,
+            RoleManager<IdentityRole> _roleManager,
+            IOptions<IdentityOptions> optionsAccessor)
+            : base(_userManager, _roleManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("FirstName", user.FirstName ?? ""));
+            identity.AddClaim(new Claim("LastName", user.LastName ?? ""));
+            return identity;
+        }
+    }
+}
